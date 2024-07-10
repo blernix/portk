8f89c602cc1efdfbae4716e9ae194e8e74215834
Killian Lecrut <killianlecrut@MacBook-Air-de-Killian.local>
2024-07-10 23:11:56 +0200

    animation hero

--- nav.js
+++ nav.js
@@ -1,3 +1,19 @@
+// CARROUSEL 
+
 document.getElementById('menu-toggle').addEventListener('click', function() {
     document.getElementById('nav-links').classList.toggle('active');
+});
+
+
+// HERO ANIMATION 
+
+document.addEventListener('scroll', function() {
+    const hero = document.querySelector('.hero');
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 350) {
+        hero.classList.add('scrolled');
+    } else {
+        hero.classList.remove('scrolled');
+    }
 });
